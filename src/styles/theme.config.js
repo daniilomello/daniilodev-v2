2eab15ba94d24c110d29951b6119f82224b5cfe2
Danilo Mello <danilodemellow@gmail.com>
2022-04-12 20:49:28 -0300

    style: defining chakra theme

--- src/styles/theme.config.js
+++ src/styles/theme.config.js
@@ -1,29 +1,42 @@
 import { extendTheme } from '@chakra-ui/react';
 
+
 export const theme = extendTheme({
+  colors: {
+    blue: {
+      900: '#04194D',
+      800: '#2D3460',
+      700: '#575A8B',
+      600: '#6B6EA0',
+      500: '#7F82B6',
+      400: '#9597CC',
+      300: '#ABACE3',
+      200: '#C1C2FA',
+      100: '#D7D8FF',
+      50: '#F7F8FF',
+    },
+    pink: {
+      900: '#641957',
+      800: '#792E6B',
+      700: '#8F427F',
+      600: '#A55694',
+      500: '#BB6BA9',
+      400: '#D280BF',
+      300: '#E995D5',
+      200: '#FFABEC',
+      100: '#FFC1FF',
+      50: '#FFD8FF',
+    },
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
   styles: {
-    colors: {
-      gray: {
-        900: '#181B23',
-        800: '#1F2029',
-        700: '#353646',
-        600: '#4B4D63',
-        500: '#616480',
-        400: '#797D9A',
-        300: '#9699B0',
-        200: '#B3B5C6',
-        100: '#D1D2DC',
-        50: '#EEEEF2',
-      },
-    },
-    fonts: {
-      heading: 'Roboto',
-      body: 'Roboto',
-    },
     global: {
       body: {
-        bg: 'gray.900',
-        color: 'gray.50',
+        bg: 'blue.900',
+        color: 'blue.50',
       },
     },
   },
