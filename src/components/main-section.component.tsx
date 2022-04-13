2f63d4f94933d77c3272a9f080ac6d904b95c257
Danilo Mello <danilodemellow@gmail.com>
2022-04-12 22:58:59 -0300

    feat: create main section component

---
+++ src/components/main-section.component.tsx
@@ -0,0 +1,26 @@
+import { Container } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+interface MainSectionProps {
+  children: ReactNode;
+}
+
+export function MainSection({ children }: MainSectionProps) {
+  return (
+    <Container
+      as="main"
+      bg="blue.900"
+      border="1px"
+      borderColor="blue.700"
+      mx="auto"
+      maxWidth="768px"
+      minHeight="600px"
+      p="42px"
+      borderRadius="24px"
+      position="relative"
+      top="-140px"
+    >
+      {children}
+    </Container>
+  );
+}
