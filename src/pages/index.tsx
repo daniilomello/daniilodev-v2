6ae43798ee8848168f0364a25bc0f7d2a0e451f1
Danilo Mello <danilodemellow@gmail.com>
2022-04-12 22:59:14 -0300

    feat: update home page components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,94 +12,18 @@ import {
 import Link from 'next/link';
 import { RiErrorWarningLine } from 'react-icons/ri';
 import { Header } from '../components/header.component';
-import { Avatar } from '../components/avatar.component';
-import { AvaliableTooltip } from '../components/avaliable-tooltip.component';
+import { AboutSection } from '../components/about-section.component';
+import { AvatarSection } from '../components/avatar-section.component';
+import { MainSection } from '../components/main-section.component';
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <Container
-        as="main"
-        bg="blue.900"
-        border="1px"
-        borderColor="blue.700"
-        mx="auto"
-        maxWidth="768px"
-        minHeight="600px"
-        p="42px"
-        borderRadius="24px"
-        position="relative"
-        top="-140px"
-      >
-        <Box
-          as="section"
-          display="flex"
-          justifyContent="space-between"
-          alignItems="flex-start"
-        >
-          <Avatar />
-          <AvaliableTooltip />
-        </Box>
-
-        <Box as="section" mt="48px">
-          <Heading as="h3" fontSize="24px" mb="24px">
-            Olá Jovem! 👋
-          </Heading>
-          <Text fontSize="18px">
-            Desenvolvedor Front-end focado em React, com pouco conhecimento em
-            Back-end com Node. Atualmente trabalhando na{' '}
-            <Link
-              href="https://agenciar8.com.br/?origin=daniilo.dev"
-              passHref={true}
-            >
-              <ChakraLink
-                as="a"
-                rel="nofollow"
-                target="_blank"
-                color="pink.200"
-                fontWeight="bold"
-              >
-                @agenciaR8
-              </ChakraLink>
-            </Link>
-            , e desenvolvendo sistema para{' '}
-            <Link
-              href="https://gabaritalab.com.br/?origin=daniilo.dev"
-              passHref={true}
-            >
-              <ChakraLink
-                as="a"
-                rel="nofollow"
-                target="_blank"
-                color="pink.200"
-                fontWeight="bold"
-              >
-                @gabaritalab
-              </ChakraLink>
-            </Link>
-            .
-          </Text>
-          <Text fontSize="18px" mt="24px">
-            Estudando Node e Typescript na{' '}
-            <Link
-              href="https://rocketseat.com.br/?origin=daniilo.dev"
-              passHref={true}
-            >
-              <ChakraLink
-                as="a"
-                rel="nofollow"
-                target="_blank"
-                color="pink.200"
-                fontWeight="bold"
-              >
-                @rocketseat
-              </ChakraLink>
-            </Link>{' '}
-            .
-          </Text>
-        </Box>
-      </Container>
+      <MainSection>
+        <AvatarSection />
+        <AboutSection />
+      </MainSection>
     </>
   );
 };
